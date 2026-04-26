e7aa9a00b8688be7778b1cdce0b990d16795506c
Tran Canh Nam <canhnam357@gmail.com>
2026-04-26 12:57:49 +0700

    first commit

---
+++ src/main/java/canhnam357/shortenurlproject/ShortenUrlProjectApplication.java
@@ -0,0 +1,13 @@
+package canhnam357.shortenurlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenUrlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenUrlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/canhnam357/shortenurlproject/ShortenUrlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package canhnam357.shortenurlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenUrlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
