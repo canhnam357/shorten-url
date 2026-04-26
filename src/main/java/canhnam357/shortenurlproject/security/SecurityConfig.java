4df6a01d77d8eb5f368377d15f4cc99a1b1fa7cc
Tran Canh Nam <canhnam357@gmail.com>
2026-04-26 13:58:51 +0700

    updaet CORS

--- src/main/java/canhnam357/shortenurlproject/security/SecurityConfig.java
+++ src/main/java/canhnam357/shortenurlproject/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://shortener-url-fe-tau.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
